51071c91ee72e18eada7ddc200d214090f7b9186
root <madhavnikhil142@gmail.com>
2022-02-09 02:10:36 +0000

    logical turtle

--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -13,19 +13,22 @@ def screen_clicked(x, y):
     print('You pressed: x=' + str(x) + ', y=' + str(y))
     
     # 6. Call the turtle .penup() method
-    
+    my_nikhil.penup()
     # 7. Move the turtle to a new location using .goto(x, y)
 
-
+    my_nikhil.goto(x, y)
 def turtle_clicked(x, y):
     print('turtle clicked!')
     
     # 8. Make a for loop to run the next instructions 3 times
-        
+    for i in range(3):
+        my_nikhil.right(360)
+        my_nikhil.color(get_random_color())
         # 9. Make the turtle spin 360 degrees using the .right() method
         
         # 10. Use the .color() method and getRandomColor() function to change
         # the color of the turtle,
+
         # myTurtle.color(get_random_color())
 
 
@@ -34,17 +37,18 @@ if __name__ == '__main__':
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    my_nikhil = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    my_nikhil.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    my_nikhil.color('green')
+    my_nikhil.pencolor('blue')
     # 4. Set and new width, length, and outline of our turtle
     #    my_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
-
+    my_nikhil.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
     # 5. Uncomment the following line and replace 'my_turtle' with your turtle
     # my_turtle.onclick(turtle_clicked)
-
+    my_nikhil.onclick(turtle_clicked)
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
     turtle.done()
