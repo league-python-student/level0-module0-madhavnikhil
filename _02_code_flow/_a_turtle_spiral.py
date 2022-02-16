2182e6fa578334dbb2f89b801cfefee73ca1916e
madhavnikhil <madhavnikhil142@gmail.com>
2022-02-16 02:02:34 +0000

    flamingninjastar

--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,23 +14,23 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
-    
+    my_Coder = turtle.Turtle()
     # This code sets our shape to a turtle
-    
-    # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    
-    # Set your turtle's color using .color('green')
-    
-    # Use a loop to repeat a the code below 50 times
-        
-        # Set the turtle color to a random color
 
+    # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
+    my_Coder.speed(0)
+    # Set your turtle's color using .color('green')
+    my_Coder.color('green')
+    # Use a loop to repeat a the code below 50 times
+    for i in range(50):
+        # Set the turtle color to a random color
+        my_Coder.color(get_random_color())
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-        
+        my_Coder.forward(5*i)
         # Turn the turtle (360/7) degrees to the right
-         
+        my_Coder.right(360/7)
         # Change the turtle width to 'i' (the loop variable)
-        
+        my_Coder.width(i)
         # Check the pattern against the picture in the recipe. If it matches, you are done!
     
 # ===================== DO NOT EDIT THE CODE BELOW ============================
